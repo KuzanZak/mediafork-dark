1a5f5b6c6fd1d5abdd97d3c3d4629994855020eb
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-15 21:17:39 +0200

    switch button ok

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,17 +1,20 @@
-// const button = document.querySelector(".nav-burger");
+const button = document.querySelector(".nav-burger");
 
-// const ulNav = document.querySelector(".nav-list")
+const ulNav = document.querySelector(".nav-list")
 
-// const ulNavMobile = document.createElement("ul")
-// const bodyHtml = document.querySelector(".template-dark")
-// const div = document.querySelector(".hero-banner")
+const ulNavMobile = document.createElement("ul")
+const bodyHtml = document.querySelector(".template-dark")
+const div = document.querySelector(".hero-banner")
 // console.log(ulNavMobile)
-// document.body.insertBefore(ulNavMobile, div)
-// // console.log(ulNavMobile)
+document.body.insertBefore(ulNavMobile, div)
+// console.log(ulNavMobile)
+const buttonI = document.querySelector(".nav-burger > i");
+console.log(buttonI)
+button.addEventListener("click", function(event){
+    ulNavMobile.innerHTML = ulNav.innerHTML
+    buttonI.classList.toggle("fa-bars");
+    buttonI.classList.toggle("fa-angle-up");
 
-// button.addEventListener("click", function(event){
-//     ulNavMobile.innerHTML = ulNav.innerHTML
-//     ulNavMobile.innerHTML.style.
-//     console.log("click")
-// })
+    console.log("click")
+})
 
