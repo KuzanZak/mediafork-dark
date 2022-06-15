5ca3df91e63edab898e49243c1e3608c1b0db7c1
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-16 00:09:48 +0200

    centrer cette foutu nav sans tricher

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -9,13 +9,20 @@ const buttonI = document.querySelector(".nav-burger > i");
 const headerContainer = document.querySelector(".header-container")
 console.log(headerContainer)
 
-
+function navAppear(){
+    if (ulNav.style.display === "none") {
+        ulNav.style.display ="flex";
+    } else {
+        ulNav.style.display = "none";
+    }
+}
 
 buttonBurger.addEventListener("click", function(event){
     
     buttonI.classList.toggle("fa-bars");
     buttonI.classList.toggle("fa-angle-up");
     // headerContainer.style.justifyContent ="flex-start";
+    navAppear()
     // ulNav.style.display ="flex";
     // ulNav.style.marginLeft ="0%"
     // console.log("click")
