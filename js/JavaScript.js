5ab9e3300833730ed9bede7ab4c00cd679714776
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-16 16:21:01 +0200

    clear js

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,15 +1,7 @@
 const buttonBurger = document.querySelector(".nav-burger");
-
 const ulNav = document.querySelector(".nav-list")
-console.log(ulNav)
-
 const buttonI = document.querySelector(".nav-burger > i");
-// console.log(buttonI)
 
-const headerContainer = document.querySelector(".header-container")
-// console.log(headerContainer)
-
-const bodyDark = document.body;
 
 function navAppear(){
     ulNav.classList.toggle("display")
@@ -22,7 +14,9 @@ function switchButton(){
 
 function overflow(){
     if (ulNav.classList.contains("display")) {
-        bodyDark.classList.add("overflow");
+        document.body.classList.add("overflow");
+    } else{
+        document.body.classList.remove("overflow");
     }
 }
 
