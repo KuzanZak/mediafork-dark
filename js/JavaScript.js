0f809944f09cc34b5e8338fe62b33bb6836324e0
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-16 16:22:07 +0200

    clear js 2.0

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -2,16 +2,6 @@ const buttonBurger = document.querySelector(".nav-burger");
 const ulNav = document.querySelector(".nav-list")
 const buttonI = document.querySelector(".nav-burger > i");
 
-
-function navAppear(){
-    ulNav.classList.toggle("display")
-}
-
-function switchButton(){
-    buttonI.classList.toggle("fa-angle-up");
-    buttonI.classList.toggle("fa-bars");
-}
-
 function overflow(){
     if (ulNav.classList.contains("display")) {
         document.body.classList.add("overflow");
@@ -21,7 +11,8 @@ function overflow(){
 }
 
 buttonBurger.addEventListener("click", function(event){
-    switchButton()
-    navAppear()
+    ulNav.classList.toggle("display")
+    buttonI.classList.toggle("fa-angle-up");
+    buttonI.classList.toggle("fa-bars");
     overflow()
 });
