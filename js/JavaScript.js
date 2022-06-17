a6fe14ab23de3a62bb73d2c0ed5ce2e9a336135f
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-06-17 10:50:27 +0200

    menu mobile and desktop ok

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,27 +1,55 @@
-const buttonBurger = document.querySelector(".nav-burger");
-const ulNav = document.querySelector(".nav-list")
-const buttonI = document.querySelector(".nav-burger > i");
+const mobileButton = document.getElementById("mobile-button");
+const mainNav = document.getElementById("main-nav");
+const mobileIcon = document.getElementById("mobile-icon");
+const navLinks = document.querySelectorAll("#main-nav a")
 
-function overflow(){
-    if (ulNav.classList.contains("display")) {
-        document.body.classList.add("overflow");
-    } else{
-        document.body.classList.remove("overflow");
-    }
-}
+// function overflow(){
+//     if (mainNav.classList.contains("display")) {
+//         document.body.classList.add("overflow");
+//     } else{
+//         document.body.classList.remove("overflow");
+//     }
+// }
 
 function transition(){
-    if (buttonI.classList.contains("fa-angle-up")){
-        ulNav.classList.add("transition")
+    if (mobileIcon.classList.contains("fa-angle-up")){
+        mainNav.classList.add("transition")
     } else{
-        ulNav.classList.remove("transition")
+        mainNav.classList.remove("transition")
     }
 }
 
-buttonBurger.addEventListener("click", function(event){
-    ulNav.classList.toggle("display")
-    buttonI.classList.toggle("fa-angle-up");
-    buttonI.classList.toggle("fa-bars");
-    overflow()
-    transition()
+function switchMobileIcon() {
+    if(mobileIcon.classList.contains("fa-bars")) {
+        mobileIcon.classList.replace("fa-bars", "fa-chevron-up");
+    } else {
+        mobileIcon.classList.replace("fa-chevron-up", "fa-bars");
+    }
+}
+
+function toggleNav(){
+    if (window.innerWidth >= 768) return; // si je suis pas sur mobile, je sors de la fonction
+    mainNav.classList.toggle("display");
+    mobileIcon.classList.toggle("fa-chevron-up");
+    mobileIcon.classList.toggle("fa-bars");
+    document.body.classList.toggle("overflow")
+    // switchMobileIcon()
+    // overflow()
+    // transition()
+};
+
+function resetNew(){
+    mainNav.classList.remove("display");
+    document.body.classList.remove("overflow");
+    mobileIcon.classList.replace("fa-chevron-up", "fa-bars")
+}
+
+mobileButton.addEventListener("click", toggleNav)
+
+mainNav.addEventListener("click", function (event){
+    if(event.target.hasAttribute("href")) toggleNav();
 });
+
+window.addEventListener("resize", function(event){
+    if (window.innerWidth >= 768) resetNew();
+})
